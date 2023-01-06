e72549f27ca9522767f8c6b779abb06530e64fe9
DianaKorladinova <chanchunche@gmail.com>
2023-01-06 08:55:25 +0100

    fixed token verification

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const {connect} = require("mongoose")
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+const { connect } = require('mongoose')
 
-connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sgpqskk.mongodb.net/?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sgpqskk.mongodb.net/?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log('MongoDB Connected'))
 
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 const booksRouter = require('./routes/books')
-// const {verify} = require("./middleware/token");
+// const { verify } = require('./middleware/token')
 
-const app = express();
+const app = express()
 
 app.use(cors({
     credentials: true,
-    origin:  (origin, callback) => {
-        if (`http://localhost:${process.env.FRONTEND_PORT}` !== origin) {
-            callback(new Error('Unknown port'))
-        } else {
-            callback(null, true)
-        }
-    }
+    origin: (origin, callback) => {
+
+        callback(null, true)
+
+    },
 }))
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/books', booksRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/books', booksRouter)
 
-module.exports = app;
+module.exports = app
--- middleware/token.js
+++ middleware/token.js
@@ -2,12 +2,13 @@ const jwt = require("jsonwebtoken");
 
 exports.verify = (req, res, next) => {
     const token = req.headers.authorization;
+    console.log(token)
     if (!token) {
         return res.status(403).json({error: 'No credentials sent!'});
     } else {
-        jwt.verify(token, process.env.SECRET, {}, (err, result) => {
+        jwt.verify(token.split(" ")[1], process.env.SECRET, {}, (err, result) => {
             if (err) {
-                return res.status(403).json({message: "Bad token"});
+                return res.status(403).json({message: "Bad token", err});
             } else {
                 const {username} = result
                 res.locals = {username}
